f2e705cd967b1ab839eba832bc87d5c14df79f5e
Audun Øhra <audunohera@gmail.com>
2018-06-06 03:18:27 +0200

    menu and interactivity
    Starting to become a page.

--- layout.js
+++ layout.js
@@ -1,8 +1,50 @@
 /******************************************
- *  Author : Audun Øhra
- *  Created On : Sun Jun 03 2018
- *  File : layout.js
+ *  Author : Audun Øhra   
+ *  Created On : Mon May 28 2018
+ *  File : Layout.js
  *******************************************/
 
-var layouts = []
+var layoutList = [2, 3, 1];
 
+var imageList = ["pictures/pic1.jpg", "1/1/4/4",
+                 "pictures/pic2.jpg", "1/4/5/9",
+                 "pictures/pic3.jpg", "4/2/7/4",
+                 "pictures/pic4.jpg", "4/1/5/2",
+                 "pictures/pic5.jpg", "4/4/7/7",
+                 "pictures/pic6.jpg", "2/9/5/12"];
+
+var prevResult = 15;
+
+function getRandomLayout() {
+    var random = Math.floor(Math.random()*layoutList.length);
+    console.log(random);
+    if(prevResult == random) {
+        getRandomLayout();
+    } else {
+        setLayoutByIndex(random);
+    }
+}
+
+function setLayoutByIndex(projectNumber) {
+    var container = document.getElementById("image-container");
+    container.innerHTML = " ";
+    var index = projectNumber*2;
+    var imgAmount = layoutList[projectNumber];
+    var imgIndex = getInitialIndex(projectNumber);
+    for(var i = imgIndex; i < (imgIndex+(imgAmount*2));i+=2) {
+        var image = document.createElement("DIV");
+        image.className += "image";
+        image.style.backgroundImage = "url(" + imageList[i] + ")";
+        image.style.gridArea = imageList[i+1];
+        container.appendChild(image);
+    }
+    prevResult = projectNumber;
+}
+
+function getInitialIndex(number) {
+    var index = 0;
+    for(var i = 0; i < number; i++) {
+        index += layoutList[i];
+    }
+    return index*2;
+}
--- script.js
+++ script.js
@@ -1,39 +1,18 @@
 /******************************************
- *  Author : Audun Øhra   
- *  Created On : Mon May 28 2018
+ *  Author : Author   
+ *  Created On : Wed Jun 06 2018
  *  File : script.js
  *******************************************/
-var imageList = ["pictures/pic1.jpg",
-                 "pictures/pic2.jpg",
-                 "pictures/pic3.jpg",
-                 "pictures/pic4.jpg",
-                 "pictures/pic5.jpg",
-                 "pictures/pic6.jpg"];
-var layoutList = [2, 3, 1];
 
-function getRandLayout() {
-    var rand = Math.floor(Math.random()*layoutList.length);
-    console.log(rand);
-    setLayoutByIndex(rand);
-}
+ var state = 0;
 
-function setLayoutByIndex(index) {
-    var container = document.getElementById("imagecontainer");
-    container.innerHTML = " ";
-    var imgAmount = layoutList[index];
-    var imgIndex = getInitialIndex(index);
-    for(var i = imgIndex; i < (imgIndex+imgAmount); i++) {
-        var image = document.createElement("IMG");
-        image.className += "img" + (i-imgIndex);
-        image.src = imageList[i];
-        container.appendChild(image);
-    }
-}
-
-function getInitialIndex(number) {
-    var index = 0;
-    for(var i = 0; i < number; i++) {
-        index += layoutList[i];
-    }
-    return index;
-}
+ function workMenu() {
+     var menu = document.getElementById("work-list");
+     if(state == 0) {
+        menu.className = "white-box position-b"
+        state = 1;
+     } else {
+        menu.className = "white-box position-a"
+        state = 0;
+     }
+ }
